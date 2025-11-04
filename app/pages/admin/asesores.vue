18fdbee1f84ef027d5704af20076446879f1257b
OneWilliam <wyvargasc@unjbg.edu.pe>
2025-11-04 08:07:11 -0500

    feat: nuevo layout para admin

---
+++ app/pages/admin/asesores.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-3xl font-bold text-gray-800 mb-6">
+            Gestión de Asesores
+        </h1>
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <p class="text-gray-600">
+                Aquí se mostrará la información de los asesores.
+            </p>
+            <!-- Contenido específico para la gestión de asesores -->
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    middleware: ["auth"],
+    layout: "admin",
+});
+</script>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8"></div>
+    <div
+        class="bg-white mx-auto px-4 sm:px-6 h-fit lg:px-8 py-8 border border-gray-200 rounded-3xl"
+    >
+        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+    </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
     middleware: "auth",
     requiredRoles: ["ADMINISTRADOR"],
-    layout: "dashboard",
+    layout: "admin",
 });
 </script>
