75a3261a5561ebc22bd8a91ff7e6f05dd3344424
OneWilliam <wyvargac@unjbg.edu.pe>
2025-12-03 01:56:01 -0500

    chore: limpieza de codigo antiguo

--- server/utils/repositories/base.ts
+++
@@ -1,7 +0,0 @@
-export interface GenericRepository<T, ID = string> {
-  findAll(): Promise<T[]>;
-  findById(id: ID): Promise<T | null>;
-  create(entity: T): Promise<T>;
-  update(id: ID, partial: Partial<T>): Promise<T | null>;
-  delete(id: ID): Promise<boolean>;
-}
